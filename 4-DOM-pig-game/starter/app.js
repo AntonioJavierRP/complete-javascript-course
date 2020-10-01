ed4d7d6bb1e7479dd3e55a332f1f034e2f0394ea
antonio javier rodriguez perez <antoniojrp1996@gmail.com>
2020-10-01 15:21:43 +0200

    new branch

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -17,8 +17,8 @@ activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+dice+'<\em>';
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+dice+'<\em>';
 
 
 var x = document.querySelector('#score-0').textContent;
